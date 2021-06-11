b8d21d683aa5ee3aa7b45ba9201e0b9d317a4f06
taejoonn <taejoonn@gmail.com>
2021-06-11 14:08:36 -0400

    matic address fix & added bsctest addresses

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,6 +1,7 @@
 import rinkebyAddresses from './addresses.json'
 import mainnetAddresses from '@zoralabs/core/dist/addresses/1.json'
 import maticTestAddresses from './maticTestAddresses.json'
+import bscTestAddresses from './bscTestAddresses.json'
 
 interface AddressBook {
   [key: string]: {
@@ -15,4 +16,5 @@ export const addresses: AddressBook = {
   rinkeby: rinkebyAddresses,
   mainnet: mainnetAddresses,
   maticTest: maticTestAddresses,
+  bscTest: bscTestAddresses,
 }
